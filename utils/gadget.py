7c3d47804172f57871ff6bdfe8c2e1ee16e02a03
Shu Yu Huang <syhuang@sted.tw>
2021-11-04 18:23:07 +0800

    Change gadget and nn_function

--- utils/gadget.py
+++ utils/gadget.py
@@ -1,27 +1,79 @@
+gadget.py
+Who has access
+
+System properties
+Type
+Text
+Size
+2 KB
+Storage used
+3 KB
+Location
+utils
+Owner
+me
+Modified
+5:28 PM by me
+Opened
+6:22 PM by me
+Created
+2:57 PM with Google Drive Web
+Add a description
+Viewers can download
+import pandas as pd
+import numpy as np
+
+import torch
+import torch.nn.functional as F
 
 import matplotlib.pyplot as plt
+import plotly.express as px
 import IPython.display as dspl
 
 
 
-def plt_once(ee,idx,**args):
-    total=len(args)
-    colors=("--r","--g","--b","--k")
+def mean_acc(pred,y):
+    return (pred.argmax(-1)==y).type(torch.float32).mean().item()
+def to_numpy(x: torch.Tensor)->np.array:
+    return x.detach().cpu().numpy()
+def show_progress(idx,display_id,**args):
     if idx>1:
-        for n,arg in enumerate(args):
-            plt.subplot(1,total,n+1)
-            if idx>100:
-                grid=int(idx//100)
-                plt.plot(list(range(0,idx+1,grid)),args[arg][::grid],colors[n%4])
-            else:
-                plt.plot(list(range(idx+1)),args[arg],colors[n%4])
-            
-            plt.title(arg)
-            plt.xlabel("batch")
-            plt.ylabel("amount")
-        dspl.update_display(plt.gcf(),display_id=2)
-        plt.cla()
-        
-def show_progress(ee,idx,**args):
-    if idx>1:
-        dspl.update_display(f"epoch:{ee},{[arg+f':{args[arg][-1]:.04f}' for arg in args]}",display_id=1)
+        dspl.update_display(f"{[arg+f':{args[arg]:.04f}' if isinstance(args[arg],float) else arg+f':{args[arg]}' for arg in args ]}",display_id=display_id)
+
+
+class Rec():
+    def __init__(self):
+        self.df=pd.DataFrame()
+    def append(self,ltn,y,itr):
+        # arc=F.normalize(ltn)
+        df_new=pd.DataFrame()
+        df_new["f1"]=to_numpy(ltn)[:,0]
+        df_new["f2"]=to_numpy(ltn)[:,1]
+        df_new["arc"]=0.5
+        df_new["iterations"]=np.repeat(itr,len(ltn))
+        df_new["label"]=to_numpy(y)
+        self.df=self.df.append(df_new)
+    def normalize(self):
+        # Normalize latent by maximum value
+        self.mx_norm=np.linalg.norm(self.df[["f1","f2"]].values,axis=1).max()
+        self.df[["f1",'f2']]=self.df[["f1",'f2']].apply(lambda x: x/self.mx_norm)
+
+        # Project latents into arc
+        df=self.df.copy()
+        norm=np.linalg.norm(df[["f1","f2"]].values,axis=1,keepdims=True)
+        df[["f1",'f2']]=df[["f1",'f2']].values/norm
+        df["arc"]=df["arc"].values/0.5*0.9
+
+        self.df=self.df.append(df)
+
+
+    def __repr__(self):
+        fig=px.scatter(self.df, x="f1", y="f2", animation_frame="iterations", color="label",opacity=self.df.arc,
+              range_x=[-1.5,1.5],
+              range_y=[-1.5,1.5], width=800, height=800)
+        fig.update_yaxes(
+            scaleanchor = "x",
+            scaleratio = 1,
+        )
+        fig.show()
+        return fig
--- utils/nn_fn.py
+++ utils/nn_fn.py
@@ -1,54 +1,31 @@
-import math
+nn_fn.py
+Who has access
+
+System properties
+Type
+Text
+Size
+6 KB
+Storage used
+13 KB
+Location
+utils
+Owner
+me
+Modified
+5:26 PM by me
+Opened
+6:22 PM by me
+Created
+2:57 PM with Google Drive Web
+Add a description
+Viewers can download
 import torch
 from torch import nn
 import torch.nn.functional as F
 
-class ArcMarginProduct(nn.Module):
-    r"""Implement of large margin arc distance: :
-        Args:
-            in_features: size of each input sample
-            out_features: size of each output sample
-            s: norm of input feature
-            m: margin
-            cos(theta + m)
-        """
-    def __init__(self, in_features, out_features, s=30.0, m=0.50, easy_margin=False):
-        super(ArcMarginProduct, self).__init__()
-        self.in_features = in_features
-        self.out_features = out_features
-        self.s = s
-        self.m = m
-        self.weight = nn.Parameter(torch.FloatTensor(out_features, in_features))
-        nn.init.xavier_uniform_(self.weight)
-
-        self.easy_margin = easy_margin
-        self.cos_m = math.cos(m)
-        self.sin_m = math.sin(m)
-        self.th = math.cos(math.pi - m)
-        self.mm = math.sin(math.pi - m) * m
-
-    def forward(self, input, label):
-        # --------------------------- cos(theta) & phi(theta) ---------------------------
-        cosine = F.linear(F.normalize(input), F.normalize(self.weight))
-        sine = torch.sqrt((1.0 - torch.pow(cosine, 2)).clamp(0, 1))
-        phi = cosine * self.cos_m - sine * self.sin_m
-        if self.easy_margin:
-            phi = torch.where(cosine > 0, phi, cosine)
-        else:
-            phi = torch.where(cosine > self.th, phi, cosine - self.mm)
-        # --------------------------- convert label to one-hot ---------------------------
-        # one_hot = torch.zeros(cosine.size(), requires_grad=True, device='cuda')
-        one_hot = torch.zeros(cosine.size(), device='cuda')
-        one_hot.scatter_(1, label.view(-1, 1).long(), 1)
-        # -------------torch.where(out_i = {x_i if condition_i else y_i) -------------
-        output = (one_hot * phi) + ((1.0 - one_hot) * cosine)  # you can use torch.where if your torch.__version__ is 0.4
-        output *= self.s
-        # print(output)
-
-        return output
-
 class AddMarginProduct(nn.Module):
-    r"""Implement of large margin cosine distance: :
+    """Implement of large margin cosine distance: :
     Args:
         in_features: size of each input sample
         out_features: size of each output sample
@@ -66,20 +43,71 @@ class AddMarginProduct(nn.Module):
         self.weight = nn.Parameter(torch.FloatTensor(out_features, in_features))
         nn.init.xavier_uniform_(self.weight)
 
-    def forward(self, input, label):
+    def forward(self, input, label=None):
         # --------------------------- cos(theta) & phi(theta) ---------------------------
         cosine = F.linear(F.normalize(input), F.normalize(self.weight))
+        if label is None:
+            return cosine
+
         phi = cosine - self.m
         # --------------------------- convert label to one-hot ---------------------------
         one_hot = torch.zeros(cosine.size(), device=label.device)
         # one_hot = one_hot.cuda() if cosine.is_cuda else one_hot
         one_hot.scatter_(1, label.view(-1, 1).long(), 1)
         # -------------torch.where(out_i = {x_i if condition_i else y_i) -------------
-        output = (one_hot * phi) + ((1.0 - one_hot) * cosine)  # you can use torch.where if your torch.__version__ is 0.4
-        output *= self.s
+        loss = (one_hot * phi) + ((1.0 - one_hot) * cosine)  # you can use torch.where if your torch.__version__ is 0.4
+        loss *= self.s
         # print(output)
 
-        return output
+        return cosine,loss
+
+    def __repr__(self):
+        return self.__class__.__name__ + '(' \
+               + 'in_features=' + str(self.in_features) \
+               + ', out_features=' + str(self.out_features) \
+               + ', s=' + str(self.s) \
+               + ', m=' + str(self.m) + ')'
+
+class ArcMarginProduct(nn.Module):
+    def __init__(self, in_feature, out_feature, s=32.0, m=0.40, easy_margin=False):
+        super(ArcMarginProduct, self).__init__()
+        self.in_feature = in_feature
+        self.out_feature = out_feature
+        self.s = s
+        self.m = m
+        self.weight = nn.Parameter(torch.Tensor(out_feature, in_feature))
+        nn.init.xavier_uniform_(self.weight,gain=1.0)
+
+        self.easy_margin = easy_margin
+        self.cos_m = math.cos(m)
+        self.sin_m = math.sin(m)
+
+        # make the function cos(theta+m) monotonic decreasing while theta in [0°,180°]
+        self.th = math.cos(math.pi - m)
+        self.mm = math.sin(math.pi - m) * m
+        self.eps = torch.tensor(1e-10,dtype=torch.float32)
+    def forward(self, x, label=None):
+        # cos(theta)
+        cosine = F.linear(F.normalize(x), F.normalize(self.weight))
+        if label is None:
+            return cosine
+
+        # cos(theta + m)
+        sine = torch.sqrt(1.0 - torch.pow(cosine, 2))
+        phi = cosine * self.cos_m - sine * self.sin_m
+
+        if self.easy_margin:
+            phi = torch.where(cosine > 0, phi, cosine)
+        else:
+            phi = torch.where((cosine - self.th) > 0, phi, cosine - self.mm)
+
+        #one_hot = torch.zeros(cosine.size(), device='cuda' if torch.cuda.is_available() else 'cpu')
+        one_hot = torch.zeros_like(cosine)
+        one_hot.scatter_(1, label.view(-1, 1), 1)
+        loss = (one_hot * phi) + ((1.0 - one_hot) * cosine)
+        loss *= self.s
+
+        return cosine,loss
 
     def __repr__(self):
         return self.__class__.__name__ + '(' \
@@ -90,42 +118,59 @@ class AddMarginProduct(nn.Module):
     
     
 class ClasModel(nn.Module):
-    def __init__(self,in_features,out_features=10,n_layers=3,latent_features=3,SoftMaxLayer=AddMarginProduct,s=30,m=0.2,mode="arc"):
+    def __init__(self,in_features,out_features=10,n_layers=3,latent_features=3,hidden_featrues=64,mode="cos",s=30,m=0.1, easy_margin=False):
         super().__init__()
-        HIDDEN=64
-        self.conv_0=nn.modules.Conv2d(in_features,HIDDEN,3,1,padding=(1,1),padding_mode='replicate')
+        self.conv_0=nn.modules.Conv2d(in_features,hidden_featrues,3,1,padding=(1,1),padding_mode='replicate')
         self.dropout_0=nn.modules.Dropout(0.2)
         
         self.convs = nn.ModuleList(
-              [nn.modules.Conv2d(HIDDEN,HIDDEN,3,1,padding=(1,1),padding_mode='replicate') for _ in range(n_layers)])
+              [nn.modules.Conv2d(hidden_featrues,hidden_featrues,3,1,padding=(1,1),padding_mode='replicate') for _ in range(n_layers)])
         self.mxpools = nn.ModuleList([nn.modules.MaxPool2d(2,2) for _ in range(n_layers)])
         self.dropouts = nn.ModuleList([nn.modules.Dropout(0.2) for _ in range(n_layers)])
         
         WIDTH=2
         self.gap=nn.AdaptiveAvgPool2d(WIDTH)
         self.flatten=nn.Flatten()
-        self.dense=nn.modules.Linear(HIDDEN*WIDTH*WIDTH,latent_features)
+        self.dense=nn.modules.Linear(hidden_featrues*WIDTH*WIDTH,latent_features)
         self.mode=mode
-            
-        self.out=nn.modules.Linear(latent_features,out_features)
-        self.softmax=SoftMaxLayer(latent_features,out_features,s=s,m=m)
+        if self.mode=="dense":
+            self.out_net=nn.modules.Linear(latent_features,out_features)
+        if self.mode=="cos":
+            self.out_net=AddMarginProduct(latent_features,out_features,s=s,m=m)
+        if self.mode=="arc":
+            self.out_net=ArcMarginProduct(latent_features,out_features,s=s,m=m,easy_margin=easy_margin)
+        
         
     def forward(self,data,label=None):
         hiddens=[self.dropout_0(F.relu(self.conv_0(data)))]
         for cnv,mxp,drp in zip(self.convs,self.mxpools,self.dropouts):
             x=F.relu(cnv(hiddens[-1]))
             x=mxp(x)
-            hiddens.append(drp(F.group_norm(x,4)))
+            hiddens.append(drp(F.group_norm(x,4,eps=1e-10)))
         hiddens.append(self.flatten(self.gap(hiddens[-1])))
         hiddens.append(self.dense(hiddens[-1]))
-        if label is None:
-            return hiddens
         
-        if self.mode=="arc":
-            pred=self.softmax(hiddens[-1],label)
+        
+        if (label is None) or (self.mode=="dense"):
+            pred=self.out_net(hiddens[-1])
         else:
-            pred=self.out(hiddens[-1])
+            pred,loss=self.out_net(hiddens[-1],label)
+            return pred,loss,hiddens
+
         return pred,hiddens
+    
+class FocalLoss(nn.Module):
+    def __init__(self, gamma=0, eps=1e-10):
+        super(FocalLoss, self).__init__()
+        self.gamma = gamma
+        self.eps = torch.tensor(eps,dtype=torch.float32)
+        self.ce = torch.nn.CrossEntropyLoss()
+
+    def forward(self, input, target):
+        logp = self.ce(input+self.eps, target)
+        p = torch.exp(-logp)
+        loss = (1 - p) ** self.gamma * logp
+        return loss.mean()
     
 class FocalLoss(nn.Module):
     def __init__(self, gamma=0, eps=1e-10):
